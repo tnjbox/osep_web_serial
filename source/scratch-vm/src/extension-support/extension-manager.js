13e11f6fa40b2865db5eae0933d83bc5afd52109
tnjbox <35487254+tnjbox@users.noreply.github.com>
2026-06-14 00:11:00 +0800

    Update extension-manager.js

--- source/scratch-vm/src/extension-support/extension-manager.js
+++ source/scratch-vm/src/extension-support/extension-manager.js
@@ -13,7 +13,7 @@ const builtinExtensions = {
     // but serves as a reference for loading core blocks as extensions.
     coreExample: () => require('../blocks/scratch3_core_example'),
     // These are the non-core built-in extensions.
-    ESP8266Joystick: () => require('../extensions/ESP8266_joystick/index.js'), // 👈 這裡一定要有逗號
+    ESP8266_joystick: () => require('../extensions/ESP8266_joystick/index.js'),
     pen: () => require('../extensions/scratch3_pen'),
     pen: () => require('../extensions/scratch3_pen'),
     wedo2: () => require('../extensions/scratch3_wedo2'),
